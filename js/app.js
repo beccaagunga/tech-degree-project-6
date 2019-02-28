d9707e138202fe3671dc7711a488fa82e3418e8a
beccaagunga <46332267+beccaagunga@users.noreply.github.com>
2019-02-27 21:24:50 -0500

    Added checkWin function, fixed disable method

--- js/app.js
+++ js/app.js
@@ -6,9 +6,12 @@ const ul = document.querySelector('#phrase ul');
 const letters = document.getElementsByClassName('letter');
 const space = document.getElementsByClassName('space');
 const buttons = document.getElementsByTagName('button');
+const title = document.querySelector('.title');
+const heart = document.getElementsByTagName('img');
+const shownLetters = document.getElementsByClassName('show');
 let missed = 0;
 
-// Hide overlay div
+// Hide overlay div to show game
 startGameBtn.addEventListener("click", () => {
   const toggle = document.getElementById('overlay');
   if (toggle.style.display === "none") {
@@ -35,7 +38,7 @@ function getRandomPhraseArray(arr) {
   return words;
 }
 
-// Do stuff any arr that is passed in, and add to `#phrase ul`
+// Do stuff to any arr that is passed in, and add to `#phrase ul`
 function addPhraseToDisplay(arr) {
   for (i = 0; i < arr.length; i++) {
     let li = document.createElement('li');
@@ -56,24 +59,55 @@ addPhraseToDisplay(phraseArray);
 function checkLetter(buttons) {
   let check = false;
 // Loop over the letters and check if they match the letter in the button the player has chosen.
-//If match, add the “show” class to the list item containing that letter,
-// Store the matching letter inside of a variable, and return that letter.
   for (i = 0; i < letters.length; i+=1) {
+//If match, add the “show” class to the list item containing that letter,
     if (buttons.textContent === letters[i].textContent.toLowerCase()) {
       check = true;
       letters[i].classList.add('show');
-
     }
   }
+// Store the matching letter inside of a variable, and return that letter.
   return check;
 }
 
-//  When a player chooses a letter, add the “chosen” class to that button so the same letter can’t be chosen twice.
-keyboard.addEventListener('click', (event) => {
-  const letterFound = checkLetter(event.target);
-  if (event.target.tagName === 'button') {
-      event.target.classList.add('chosen');
-      event.target.disabled = true;
+// Each time the player guesses a letter, this function will check whether the game has been won or lost.
+function checkWin() {
+  const show = document.querySelectorAll('.show');
+    if (missed === 5) {
+      overlay.style.display = '';
+      overlay.className = '';
+      overlay.classList.add('lose');
+      title.innerHTML= 'You lost! Try again?';
+      overlay.style.backgroundColor = 'maroon';
+      startGameBtn.textContent = 'Try Again';
+    } else if (show.length === letters.length) {
+      overlay.style.display = '';
+      overlay.className = '';
+      overlay.classList.add('win');
+      title.innerHTML= 'You won! Play Again?';
+      startGameBtn.textContent = 'Play Again';
+        }
     }
 
+
+//  When a player chooses a letter, add the “chosen” class to that button so the same letter can’t be chosen twice.
+keyboard.addEventListener('click', (event) => {
+const letterFound = checkLetter(event.target);
+  if (event.target.tagName === 'BUTTON') {
+      event.target.className = 'chosen';
+      event.target.disabled = true;
+
+    }
+// Remove a heart for wrong click
+    if (letterFound === false && missed < 5) {
+      heart[missed].setAttribute('src', 'images/lostHeart.png');
+   			missed++;
+      }
+// Reload game from 'win' or 'loss' screen
+      startGameBtn.addEventListener('click', () => {
+        if (startGameBtn.innerHTML === 'Play Again' || 'Try Again') {
+            location.reload();
+          }
+});
+    checkWin();
 });
